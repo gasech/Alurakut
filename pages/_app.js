d79e8a49e52a1e2bcce1c411758f04407f0011fd
Gabriel <gsechaus@gmail.com>
2021-07-13 00:43:04 -0300

    Inicio do Alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { AlurakutStyles } from '../src/components/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  /* Reset CSS */
+  
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100hv;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  
+  ${AlurakutStyles}
 `
 
 const theme = {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,60 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid';
+import Box from '../src/components/Box';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/components/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(properties){
+  return (
+    <Box>
+      <img src={`https://github.com/${properties.githubUser}.png`} style={{borderRadius:'8px'}}/>
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const randomUser = 'gasech';
+  const randomFriends = ['eduzznet','math-eusp'];
+
+  return (
+    <>
+    <AlurakutMenu/>
+
+    <MainGrid>
+
+      <div className="profileArea" style={{gridArea: 'profileArea'}}>
+        <ProfileSidebar githubUser={randomUser}/>
+      </div>
+
+      <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+        <Box>
+          <h2 className="title">Bem Vindo(a)</h2>
+          <OrkutNostalgicIconSet/>
+        </Box>
+      </div>
+      
+      <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle">Amigos {randomFriends.length}</h2>
+          <ul>
+              {randomFriends.map((index) => {
+                return (
+                  <li>
+                    <a href={`/users/${index}`} key={index}>
+                      <img src={`https://github.com/${index}.png`} />
+                      <span>{index}</span>
+                    </a>
+                  </li>
+                )
+              })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
+        
+        <Box>
+          <h2 className="smallTitle">Comunidades</h2>
+        </Box>
+        
+      </div>
+    </MainGrid>
+    </>
+  )
 }
